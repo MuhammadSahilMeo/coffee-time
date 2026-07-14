25c194064cbfd3af162bfb1787a14c47938200e5
Sahil Meo <contact.sahilmeo@gmail.com>
2026-07-14 23:57:45 +0500

    Remove extra route from api file

--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,3 @@ use App\Http\Controllers\EmployeeController;
 Route::post('/create-employee', [EmployeeController::class, 'store']);
 Route::get('/all-employee', [EmployeeController::class, 'index']);
 Route::get('/single-employee/{id}', [EmployeeController::class, 'show']);
-Route::get('/single-employee/{id}', [EmployeeController::class, 'show']);
-Route::get('/single-employee/{id}', [EmployeeController::class, 'show']);
